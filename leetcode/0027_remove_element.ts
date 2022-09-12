c0b733796c77f19f1e3bf4cc39404400b32c1a55
xnacly <47723417+xNaCly@users.noreply.github.com>
2022-09-12 09:52:42 +0200

    feat: +0027

---
+++ leetcode/0027_remove_element.ts
@@ -0,0 +1,12 @@
+let removeElement = (n: Array<number>, v: number) => {
+	let c = 0;
+	for (let i = 0; i < n.length; i++) {
+		if (n[i] !== v) {
+			n[c++] = n[i];
+		}
+	}
+	return c;
+};
+
+console.log(removeElement([3, 2, 2, 3], 3));
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
