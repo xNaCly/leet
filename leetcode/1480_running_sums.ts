076d588fb8fa48134e84fa5ead8cfa7d730f0b06
xnacly <47723417+xNaCly@users.noreply.github.com>
2022-09-13 09:48:25 +0200

    feat: +1480

---
+++ leetcode/1480_running_sums.ts
@@ -0,0 +1,12 @@
+let runningSum = (n: number[]) => {
+	let t = 0;
+	for (let i = 0; i < n.length; i++) {
+		t += n[i];
+		n[i] = t;
+	}
+	return n;
+};
+
+console.log(runningSum([1, 2, 3, 4]));
+console.log(runningSum([1, 1, 1, 1, 1]));
+console.log(runningSum([3, 1, 2, 10, 1]));
