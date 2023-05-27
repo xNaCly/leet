d4bd698233a7d1bd45ca7530573de09bf7ae6722
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-05-27 16:28:18 +0200

    f

--- leetcode/0065_valid_number.go
+++ leetcode/0065_valid_number.go
@@ -1,43 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"regexp"
-	"strconv"
-	"strings"
+	"leetcode/leetcode_helpers"
 )
 
-// returns true if not really a number
-func _checkEdge(s string) bool {
-    r, _ := regexp.Compile("\\+|-")
-    s = r.ReplaceAllString(s, "")
-    if strings.HasPrefix(s, "i") || strings.HasPrefix(s, "n"){
-        return true
-    } else if strings.Count(s, "e") > 1 {
-        return true
-    }
-    return false
-}
-
 func isNumber(s string) bool {
-	s = strings.ToLower(s)
-    if _checkEdge(s) {
-        return false
-    } else if strings.Contains(s, "e") {
-        split := strings.Split(s, "e")
-        _, err := strconv.ParseInt(split[1], 10, 64)
-        _, err1 := strconv.ParseFloat(split[0], 64)
-        return err == nil && err1 == nil
-    } else {
-        _, err := strconv.ParseFloat(s, 64)
-        return err == nil
-    }
+	var v bool
+	return v
 }
 
 func main() {
-	ints := []string{"2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789",
-		"abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53", "Infinity", "inf", "NaN", "+Inf", "-inf", "0e1", "7e69e"}
-	for _, integer := range ints {
-		fmt.Printf("%s: %t\n", integer, isNumber(integer))
+	ints := map[string]bool{
+		"2":            true,
+		"0089":         true,
+		"-0.1":         true,
+		"+3.14":        true,
+		"4.":           true,
+		"-.9":          true,
+		"2e10":         true,
+		"-90E3":        true,
+		"3e+7":         true,
+		"+6e-1":        true,
+		"53.5e93":      true,
+		"-123.456e789": true,
+		"0e1":          true,
+		"abc":          false,
+		"1a":           false,
+		"1e":           false,
+		"e3":           false,
+		"99e2.5":       false,
+		"--6":          false,
+		"-+3":          false,
+		"95a54e53":     false,
+		"Infinity":     false,
+		"inf":          false,
+		"NaN":          false,
+		"+Inf":         false,
+		"-inf":         false,
+		"7e69e":        false,
+	}
+
+	for k, v := range ints {
+		leetcode_helpers.AssertEquals(v, isNumber(k), k)
 	}
 }
