d411c099908099680045be4adaa3573c8d69d2c8
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-05-30 08:41:05 +0200

    ~0050

--- go/0050.go
+++ go/0050.go
@@ -4,37 +4,38 @@ import (
 	"math"
 )
 
-// func myPow(x float64, n int) float64 {
-// 	var neg bool
-// 	r := 1.0
-
-// 	if n == 0 {
-// 		return 1.0
-// 	} else if math.Abs(math.Abs(x)-1.0) < 1e-10 {
-// 		if x < 0 && n < 0 {
-// 			return 1.0
-// 		} else if x < 0 {
-// 			return -1.0
-// 		}
-// 		return 1.0
-// 	} else if n == 1 {
-// 		return x
-// 	} else if n < 0 {
-// 		n = int(math.Abs(float64(n)))
-// 		neg = true
-// 	}
-
-// 	for i := 1; i <= n; i++ {
-// 		r *= x
-// 	}
-
-// 	if neg {
-// 		return 1.0 / r
-// 	} else {
-// 		return r
-// 	}
-// }
-
 func myPow(x float64, n int) float64 {
-	return math.Pow(x, float64(n))
+	var neg bool
+	r := 1.0
+
+	if n == 0 {
+		return 1.0
+	} else if math.Abs(math.Abs(x)-1.0) < 1e-10 {
+		if x < 0 && n < 0 {
+			return 1.0
+		} else if x < 0 {
+			return -1.0
+		}
+		return 1.0
+	} else if n == 1 {
+		return x
+	} else if n < 0 {
+		n = int(math.Abs(float64(n)))
+		neg = true
+	}
+
+	for i := 1; i <= n; i++ {
+		r *= x
+	}
+
+	if neg {
+		return 1.0 / r
+	} else {
+		return r
+	}
 }
+
+// INFO: 400x faster than the above, but i only understand 50% of the implementation
+// func myPow(x float64, n int) float64 {
+// 	return math.Pow(x, float64(n))
+// }
