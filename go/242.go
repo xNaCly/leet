6cfb30bb0bd3a5c14ab1597c2d4739d485184af7
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-05-31 09:04:00 +0200

    +242 faster impl & benchmarks

--- go/242.go
+++ go/242.go
@@ -1,7 +1,44 @@
 package main
 
-
 func isAnagram(s string, t string) bool {
+	if len(t) != len(s) {
+		return false
+	}
+	ms := make(map[rune]uint)
+	mt := make(map[rune]uint)
+
+	sr := []rune(s)
+	tr := []rune(t)
+
+	for i := 0; i < len(s); i++ {
+		ccs := sr[i]
+		cct := tr[i]
+		if _, ok := ms[ccs]; ok {
+			ms[ccs]++
+		} else {
+			ms[ccs] = 1
+		}
+		if _, ok := mt[cct]; ok {
+			mt[cct]++
+		} else {
+			mt[cct] = 1
+		}
+	}
+
+	r := true
+	for key, value := range ms {
+		if v, ok := mt[key]; ok {
+			if v != value {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+	return r
+}
+
+func isAnagramOld(s string, t string) bool {
 	if len(t) != len(s) {
 		return false
 	}
--- go/242_test.go
+++ go/242_test.go
@@ -17,3 +17,31 @@ func Test242(t *testing.T) {
 		helper.AssertEquals(isAnagram(input[0], input[1]), expected[i], input)
 	}
 }
+
+func Benchmark242(b *testing.B) {
+	for j := 0; j < b.N; j++ {
+		inputs := [][]string{
+			{"anagram", "nagaram"},
+			{"rat", "car"},
+			{"aacc", "ccac"},
+		}
+		expected := []bool{true, false, false}
+		for i, input := range inputs {
+			helper.AssertEquals(isAnagram(input[0], input[1]), expected[i], input)
+		}
+	}
+}
+
+func Benchmark242Old(b *testing.B) {
+	for j := 0; j < b.N; j++ {
+		inputs := [][]string{
+			{"anagram", "nagaram"},
+			{"rat", "car"},
+			{"aacc", "ccac"},
+		}
+		expected := []bool{true, false, false}
+		for i, input := range inputs {
+			helper.AssertEquals(isAnagramOld(input[0], input[1]), expected[i], input)
+		}
+	}
+}
