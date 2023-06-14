fbd3596d3f9faf275bf1f6fe876a564baa1b3607
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-06-14 20:45:17 +0200

    feat: helper.AssertIntArrayEquals

--- java/helper/Helper.java
+++ java/helper/Helper.java
@@ -34,4 +34,34 @@ public class Helper {
                     Arrays.stream(b).map(i -> i.toString()).collect(Collectors.joining(", "))));
         }
     }
+
+    public static void AssertIntArrayEquals(int[] a, int[] b) {
+        boolean r = false;
+        if (a.length != b.length) {
+            r = true;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                r = true;
+                break;
+            }
+        }
+
+        StringBuilder sa = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < a.length; i++) {
+            sa.append(a[i]);
+            sb.append(b[i]);
+            if (i + 1 != a.length) {
+                sa.append(',');
+                sb.append(',');
+            }
+        }
+        if (r) {
+            throw new AssertionError(String.format("{%s} not equal to {%s}", sa, sb));
+        } else {
+            System.out.println(String.format("{%s} not equal to {%s}", sa, sb));
+        }
+    }
 }
