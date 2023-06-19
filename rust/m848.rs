df609935831bd3b0f066fefaa71a85d82e451644
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-06-19 17:52:29 +0200

    +848

--- rust/m848.rs
+++ rust/m848.rs
@@ -2,20 +2,17 @@
 trait Solution {}
 impl dyn Solution {
     pub fn shifting_letters(s: String, shifts: Vec<i32>) -> String {
-        let mut r: Vec<char> = Vec::new();
-        for (i, c) in s.chars().enumerate() {
-            let shift = shifts.get(i).unwrap();
-            r.push(c);
-            r = r
-                .iter()
-                .map(|x| {
-                    let chr = *x as u32;
-                    let offset = if chr < 97 { 97 } else { chr - 97 };
-                    char::from_u32(((offset + *shift as u32) % 26) + 97).unwrap()
-                })
-                .collect::<Vec<char>>();
-        }
-        r.iter().collect()
+        s.bytes()
+            .zip(&shifts)
+            .scan(
+                shifts.iter().fold(0, |acc, &x| (acc + x) % 26),
+                |state, (u, shift)| {
+                    let s = *state as u8;
+                    *state = (*state - shift).rem_euclid(26);
+                    Some((b'a' + (u - b'a' + s) % 26) as char)
+                },
+            )
+            .collect()
     }
 }
 
@@ -29,8 +26,9 @@ fn main() {
 
     for (i, v) in inputs.iter().enumerate() {
         let sol = <dyn Solution>::shifting_letters(String::from(v.0), v.1.to_vec());
-        if sol != expected.get(i).unwrap().to_string() {
-            println!("{sol}");
+        let exp = expected.get(i).unwrap().to_string();
+        if sol != exp {
+            panic!("{} != {}", sol, exp);
         }
     }
 }
