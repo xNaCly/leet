9c391e35e2da2a8afcfae5b19c583d6a4090f85b
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-07-03 07:44:45 +0200

    replaced \n with implicit \n from tcc

--- codegolf/ascii.c
+++ codegolf/ascii.c
@@ -1,1 +1,2 @@
-i;main(j,c){puts("   2 3 4 5 6 7\n -------------");for(;i<16;i++){printf("%X: ",i);for(j=0;j<6;j++)c=i+32+(16*j),c==127?puts("DEL"):printf("%c ",c);puts("");}}
+i;main(j,c){puts("   2 3 4 5 6 7
+-------------");for(;i<16;i++){printf("%X: ",i);for(j=0;j<6;j++)c=i+32+(16*j),c==127?puts("DEL"):printf("%c ",c);puts("");}}
