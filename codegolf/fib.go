446ceaea9bafdaaaef1c3553416d331ed9da4277
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-07-05 08:22:08 +0200

    pascals triangle and fib in go

---
+++ codegolf/fib.go
@@ -0,0 +1,1 @@
+package main;import."fmt";func main(){s,i:=[31]int{},0;for;i<31;i++{if i<2{s[i]=i}else{s[i]=s[i-1]+s[i-2]};Println(s[i])}}
---
+++ codegolf/pascal.py
@@ -0,0 +1,1 @@
+a=[1];exec("print(' '.join(map(str,a)));a=list(map(sum,zip([0]+a,a+[0])));"*20)
