260de51607bcc78dfbcb79c7fe21aa5f04130f47
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-08-03 08:23:17 +0200

    refactor(1576.go): decreased runtime by 1.5x

--- go/1576.go
+++ go/1576.go
@@ -1,10 +1,11 @@
 package main
 
-func modifyString(s string) string {
+import "strings"
+
+func modifyStringThird(s string) string {
 	ls := len(s)
-	b := make([]byte, 0)
-	for i, r := range s {
-		by := byte(r)
+	b := make([]byte, ls)
+	for i, r := range []byte(s) {
 		lb := len(b)
 		if r == '?' {
 			var rr byte
@@ -12,9 +13,44 @@ func modifyString(s string) string {
 				(ls > i+1 && rr+97 == s[i+1]) {
 				rr++
 			}
-			by = rr + 97
+			r = rr + 97
 		}
-		b = append(b, by)
+		b[i] = r
 	}
 	return string(b)
+}
+func modifyStringSecond(s string) string {
+	b := strings.Builder{}
+	for i, r := range s {
+		if r == '?' {
+			rr := 0
+			for (b.Len() > 0 && rr+97 == int(b.String()[b.Len()-1])) ||
+				(len(s) > i+1 && rr+97 == int(s[i+1])) {
+				rr++
+			}
+			b.WriteRune(rune(rr + 97))
+		} else {
+			b.WriteRune(r)
+		}
+	}
+	return b.String()
+}
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func modifyStringFirst(s string) string {
+	b := strings.Builder{}
+	for i, r := range s {
+		if r == '?' {
+			rr := 0
+			for (b.Len() > 0 && alphabet[rr] == b.String()[b.Len()-1]) ||
+				(len(s) > i+1 && alphabet[rr] == s[i+1]) {
+				rr++
+			}
+			b.WriteByte(alphabet[rr])
+		} else {
+			b.WriteRune(r)
+		}
+	}
+	return b.String()
 }
--- go/1576_test.go
+++ go/1576_test.go
@@ -16,6 +16,54 @@ func Test1576(t *testing.T) {
 	}
 
 	for _, s := range i {
-		helper.AssertFalse(strings.ContainsRune(modifyString(s), '?'))
+		t.Run(s, func(t *testing.T) {
+			t.Run("first", func(t *testing.T) {
+				helper.AssertFalse(strings.ContainsRune(modifyStringFirst(s), '?'))
+			})
+			t.Run("second", func(t *testing.T) {
+				helper.AssertFalse(strings.ContainsRune(modifyStringSecond(s), '?'))
+			})
+			t.Run("third", func(t *testing.T) {
+				helper.AssertFalse(strings.ContainsRune(modifyStringThird(s), '?'))
+			})
+		})
+	}
+}
+
+var inputSizes = map[string]int{
+	"size10":     10,
+	"size100":    100,
+	"size1000":   1000,
+	"size10000":  10000,
+	"size100000": 100000,
+}
+
+func BenchmarkFirst(b *testing.B) {
+	for size, v := range inputSizes {
+		b.Run(size, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				modifyStringFirst(strings.Repeat("j?qg??b", v))
+			}
+		})
+	}
+}
+
+func BenchmarkSecond(b *testing.B) {
+	for size, v := range inputSizes {
+		b.Run(size, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				modifyStringSecond(strings.Repeat("j?qg??b", v))
+			}
+		})
+	}
+}
+
+func BenchmarkThird(b *testing.B) {
+	for size, v := range inputSizes {
+		b.Run(size, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				modifyStringThird(strings.Repeat("j?qg??b", v))
+			}
+		})
 	}
 }
